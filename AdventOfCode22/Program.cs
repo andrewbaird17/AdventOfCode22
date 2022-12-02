35bd77c7bf3bcd0f73b085db5783703f45c07487
Andrew Baird <baird.andrew4@gmail.com>
2022-12-02 15:58:27 -0600

    Add project files.

---
+++ AdventOfCode22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
